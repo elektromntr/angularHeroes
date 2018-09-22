5fa1c903de357ed2ebb2f1e97685798bfc8f9f53
elektromntr <bartosztadeuszwojcik@gmail.com>
2018-09-22 22:24:50 +0200

    Displaying a List (not working)

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+    {id:11, name: 'Leon'},
+    {id:12, name: 'Pola'},
+    {id:13, name: 'Olek'},
+    {id:14, name: 'Maciek'},
+    {id:15, name: 'Maryśka'},
+    {id:16, name: 'Teo'},
+];
