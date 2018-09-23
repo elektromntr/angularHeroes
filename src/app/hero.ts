572118ea2c53474ba9ef9071e6deb6b4f60946a6
elektromntr <bartosztadeuszwojcik@gmail.com>
2018-09-23 09:50:44 +0200

    Master and Detail component (working)

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,4 +1,11 @@
 export class Hero {
     id: number;
     name: string;
+    /*age: number;
+
+    fitness: number;
+    mind: number;
+    social: number;
+
+    city: string;*/
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,18 +8,17 @@ import { HEROES} from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  
   heroes = HEROES;
+
+  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  selectedHero: Hero;
+  
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
